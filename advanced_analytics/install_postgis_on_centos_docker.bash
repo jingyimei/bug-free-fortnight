3427b98813fa2b7c29bd3ea34b2a6d56c3ccde50
Jingyi Mei <jmei@pivotal.io>
2017-12-01 12:22:41 +0800

    modify gdal configure prefix for greenplum and add make -j5

--- advanced_analytics/install_postgis_on_centos_docker.bash
+++ advanced_analytics/install_postgis_on_centos_docker.bash
@@ -8,19 +8,18 @@ sudo wget http://download.osgeo.org/gdal/1.11.1/gdal-1.11.1.tar.gz
 
 sudo tar zxf gdal-1.11.1.tar.gz
 cd gdal-1.11.1
-sudo ./configure
+sudo ./configure --prefix=$GPHOME
 sudo make
 sudo make install
-sudo sed -i '/LD_LIBRARY_PATH/s/LD_LIBRARY_PATH=/LD_LIBRARY_PATH=\/usr\/local\/lib:/g' /build/install/greenplum_path.sh
 
 source /build/install/greenplum_path.sh
 source /build/gpdb/gpAux/gpdemo/gpdemo-env.sh
 
 git clone --shared /workspace/geospatial /build/geospatial
 cd /build/geospatial/postgis/build/postgis-2.1.5/
-./configure --with-pgconfig=/build/install/bin/pg_config --with-raster --without-topology --prefix=$GPHOME --with-gdalconfig=/usr/local/bin/gdal-config
+./configure --with-pgconfig=/build/install/bin/pg_config --with-raster --without-topology --prefix=$GPHOME
 
-CFLAGS="-O0 -g3" CXXFLAGS="-O0 -g3" make USE_PGXS=1 all
+CFLAGS="-O0 -g3" CXXFLAGS="-O0 -g3" make USE_PGXS=1 all -j5
 sudo make install
 
 make check
